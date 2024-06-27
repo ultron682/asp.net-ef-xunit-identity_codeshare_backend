e3d37067f5a7f4c6facc510d07afeaec48d9afe2
ultron682 <52131708+ultron682@users.noreply.github.com>
2024-06-27 15:37:32 +0200

    before entity framework

--- CodeShareBackend/CodeShareHub.cs
+++ CodeShareBackend/CodeShareHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace CodeShareBackend
@@ -7,6 +8,7 @@ namespace CodeShareBackend
     {
         public async Task SendCode(string code)
         {
+            Debug.WriteLine(code);
             await Clients.Others.SendAsync("ReceiveCode", code);
         }
     }
