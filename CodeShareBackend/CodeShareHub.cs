0d3bcb351aa08c3ce3d7835fdd3ce977d207d6f6
ultron682 <52131708+ultron682@users.noreply.github.com>
2024-08-12 14:21:08 +0200

    before remove CurrentDocumentChanges

--- CodeShareBackend/CodeShareHub.cs
+++ CodeShareBackend/CodeShareHub.cs
@@ -2,17 +2,18 @@
 using CodeShareBackend.Data;
 using CodeShareBackend.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 
 public class CodeShareHub : Hub
 {
     private readonly ApplicationDbContext _context;
     private readonly UserManager<User> _userManager;
-    private static Dictionary<string, string> _connectionsNgroup = new Dictionary<string, string>();
 
+    private static Dictionary<string, string> _connectionsNgroup = [];
 
-    private static Dictionary<string, Text> Documents = [];
-    private static Dictionary<string, List<ChangeSet>> DocumentChanges = [];
+    private static Dictionary<string, Text> CurrentOpenDocuments = [];
+    private static Dictionary<string, List<ChangeSet>> CurrentDocumentChanges = [];
 
     public CodeShareHub(ApplicationDbContext context, UserManager<User> userManager)
     {
@@ -22,26 +23,65 @@ public class CodeShareHub : Hub
 
     public async Task JoinDocument(string uniqueId)
     {
-        if (!Documents.ContainsKey(uniqueId))
+        Console.WriteLine("JoinDocument: " + uniqueId + " : " + Context.ConnectionId);
+
+        if (_connectionsNgroup.ContainsKey(Context.ConnectionId))
         {
-            Documents[uniqueId] = new Text("Start document");
-            DocumentChanges[uniqueId] = new List<ChangeSet>();
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, _connectionsNgroup[Context.ConnectionId]);
+            _connectionsNgroup.Remove(Context.ConnectionId);
+        }
+        _connectionsNgroup.Add(Context.ConnectionId, uniqueId);
+
+        await Groups.AddToGroupAsync(Context.ConnectionId, uniqueId);
+        printConnectionsNgroup();
+
+        var snippet = await _context.CodeSnippets.Include(l => l.SelectedLang).SingleOrDefaultAsync(s => s.UniqueId == uniqueId);
+
+        if (!CurrentOpenDocuments.ContainsKey(uniqueId))
+        {
+            CurrentOpenDocuments[uniqueId] = new Text(snippet == null ? string.Empty : snippet.Code!);
+            CurrentDocumentChanges[uniqueId] = [];
         }
 
-        await Clients.Caller.SendAsync("ReceiveDocument", Documents[uniqueId].ToString(), DocumentChanges[uniqueId]);
+        await Clients.Caller.SendAsync("ReceiveDocument", snippet);
     }
 
-    public async Task PushUpdate(string uniqueId, string changeSetJson)
+    public async Task PushUpdate(string changeSetJson, string userId)
     {
+        if (_connectionsNgroup.ContainsKey(Context.ConnectionId) == false)
+        {
+            Console.WriteLine("Connection not in group");
+            return;
+        }
+
         var changeSet = ChangeSet.FromJSON(changeSetJson);
         if (changeSet != null)
         {
-            var document = Documents[uniqueId].ApplyChangeSet(changeSet);
+            var uniqueId = _connectionsNgroup[Context.ConnectionId];
+            //Console.WriteLine(uniqueId);
 
-            Documents[uniqueId] = document;
-            DocumentChanges[uniqueId].Add(changeSet);
+            var document = CurrentOpenDocuments[uniqueId].ApplyChangeSet(changeSet);
 
-            await Clients.OthersInGroup(uniqueId).SendAsync("ReceiveUpdate", changeSetJson);
+            CurrentOpenDocuments[uniqueId] = document;
+            CurrentDocumentChanges[uniqueId].Add(changeSet);
+
+            if (_connectionsNgroup.ContainsKey(Context.ConnectionId))
+                await Clients.OthersInGroup(uniqueId).SendAsync("ReceiveUpdate", changeSetJson);
+
+            var snippet = await _context.CodeSnippets.Include(u => u.SelectedLang).SingleOrDefaultAsync(s => s.UniqueId == uniqueId);
+            if (snippet == null)
+            {
+                //Console.WriteLine("userId: " + userId);
+                snippet = new CodeSnippet { UniqueId = uniqueId, Code = document.ToString(), UserId = (userId == string.Empty ? null : userId) };
+                _context.CodeSnippets.Add(snippet);
+            }
+            else
+            {
+                snippet.Code = document.ToString();
+                _context.CodeSnippets.Update(snippet);
+            }
+
+            await _context.SaveChangesAsync();
         }
         else
         {
@@ -49,145 +89,6 @@ public class CodeShareHub : Hub
         }
     }
 
-    public async Task SubscribeDocument(string uniqueId)
-    {
-        await Groups.AddToGroupAsync(Context.ConnectionId, uniqueId);
-    }
-
-    public async Task UnsubscribeDocument(string uniqueId)
-    {
-        await Groups.RemoveFromGroupAsync(Context.ConnectionId, uniqueId);
-    }
-
-
-    //public async Task<CodeSnippet> JoinGroup(string uniqueId)
-    //{
-    //    Console.WriteLine("JoinGroup: " + uniqueId + " : " + Context.ConnectionId);
-    //    if (_connectionsNgroup.ContainsKey(Context.ConnectionId))
-    //    {
-    //        await Groups.RemoveFromGroupAsync(Context.ConnectionId, _connectionsNgroup[Context.ConnectionId]);
-    //        _connectionsNgroup.Remove(Context.ConnectionId);
-    //    }
-    //    _connectionsNgroup.Add(Context.ConnectionId, uniqueId);
-    //    printConnectionsNgroup();
-
-    //    await Groups.AddToGroupAsync(Context.ConnectionId, uniqueId);
-
-    //    var snippet = await _context.CodeSnippets.Include(l => l.SelectedLang).SingleOrDefaultAsync(s => s.UniqueId == uniqueId);
-    //    //Console.WriteLine(snippet?.Code);
-    //    return snippet;
-    //}
-
-    //public async Task BroadcastText(string uniqueId, string code, string userId)
-    //{
-    //    var snippet = await _context.CodeSnippets.Include(u => u.SelectedLang).SingleOrDefaultAsync(s => s.UniqueId == uniqueId);
-    //    if (snippet == null)
-    //    {
-    //        //Console.WriteLine("userId: " + userId);
-    //        snippet = new CodeSnippet { UniqueId = uniqueId, Code = code, UserId = userId == string.Empty ? null : userId };
-    //        _context.CodeSnippets.Add(snippet);
-    //    }
-    //    else
-    //    {
-    //        snippet.Code = code;
-    //        _context.CodeSnippets.Update(snippet);
-    //    }
-    //    await _context.SaveChangesAsync();
-
-    //    if (_connectionsNgroup.ContainsKey(Context.ConnectionId))
-    //    {
-    //        //Console.WriteLine("wyslano: " + code);
-    //        await Clients.OthersInGroup(_connectionsNgroup[Context.ConnectionId]).SendAsync("ReceivedCode", code);
-    //    }
-    //}
-
-    //public async Task UpdateSnippetLine(string uniqueId, int lineNumber, string newLineContent)
-    //{
-    //    var snippet = await _context.CodeSnippets.SingleOrDefaultAsync(s => s.UniqueId == uniqueId);
-
-    //    if (snippet == null)
-    //    {
-    //        throw new HubException("Snippet not found");
-    //    }
-
-    //    // Split code into lines
-    //    var lines = snippet.Code.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
-
-    //    // Check if lineNumber is valid
-    //    if (lineNumber < 0 || lineNumber >= lines.Length)
-    //    {
-    //        throw new HubException("Line number is out of range.");
-    //    }
-
-    //    // Update the specified line
-    //    lines[lineNumber] = newLineContent;
-    //    Console.WriteLine(newLineContent + " on " + lineNumber);
-
-    //    // Join lines back into a single string
-    //    snippet.Code = string.Join(Environment.NewLine, lines);
-
-    //    _context.CodeSnippets.Update(snippet);
-    //    await _context.SaveChangesAsync();
-
-    //    if (_connectionsNgroup.ContainsKey(Context.ConnectionId))
-    //    {
-    //        await Clients.OthersInGroup(_connectionsNgroup[Context.ConnectionId]).SendAsync("ReceivedCode", snippet.Code);
-    //    }
-    //}
-
-    //public async Task UpdateSnippetLine(string uniqueId, string userId, int lineNumber, string newLineContent)
-    //{
-    //    //Console.WriteLine("UpdateSnippetLine" + uniqueId + " " + lineNumber + " " + newLineContent);
-    //    var snippet = await _context.CodeSnippets.SingleOrDefaultAsync(s => s.UniqueId == uniqueId);
-
-    //    //if (snippet == null)
-    //    //{
-    //    //    throw new HubException("Snippet not found");
-    //    //}
-    //    if (snippet == null)
-    //    {
-    //        //Console.WriteLine("userId: " + userId);
-    //        snippet = new CodeSnippet { UniqueId = uniqueId, Code = newLineContent, UserId = userId == string.Empty ? null : userId };
-    //        _context.CodeSnippets.Add(snippet);
-
-    //        Console.WriteLine(newLineContent + " created new " + lineNumber);
-    //    }
-    //    else
-    //    {
-    //        // Split code into lines
-    //        var lines = snippet!.Code!.Split(new[] { Environment.NewLine }, StringSplitOptions.None).ToList();
-
-    //        //foreach (var item in lines)
-    //        //{
-    //        //    Console.WriteLine(item);
-    //        //}
-
-    //        if (lines.Count > 0 && lineNumber < lines.Count)
-    //        {
-    //            // Update the specified line
-    //            lines[lineNumber] = newLineContent;
-    //            Console.WriteLine(newLineContent + " on line " + lineNumber);
-    //        }
-    //        else
-    //        {
-    //            lines.Add(newLineContent);
-    //            Console.WriteLine(newLineContent + " added on line" + lineNumber);
-    //        }
-
-    //        snippet.Code = string.Join(Environment.NewLine, lines);
-
-    //        //Console.WriteLine("\nChanged to: \n" + snippet.Code);
-
-    //        _context.CodeSnippets.Update(snippet);
-    //    }
-
-    //    await _context.SaveChangesAsync();
-
-    //    if (_connectionsNgroup.ContainsKey(Context.ConnectionId))
-    //    {
-    //        await Clients.OthersInGroup(_connectionsNgroup[Context.ConnectionId]).SendAsync("ReceivedNewLineCode", lineNumber, newLineContent);
-    //    }
-    //}
 
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
@@ -203,11 +104,11 @@ public class CodeShareHub : Hub
 
     void printConnectionsNgroup()
     {
-        Console.WriteLine("--------------------------");
+        Console.WriteLine("\n--------------------------");
         foreach (var item in _connectionsNgroup)
         {
             Console.WriteLine(item.Value + " : " + item.Key);
         }
-        Console.WriteLine("--------------------------");
+        Console.WriteLine("--------------------------\n");
     }
 }
