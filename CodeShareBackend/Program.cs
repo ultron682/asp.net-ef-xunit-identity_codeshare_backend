5c0686a0ca70585ed47cb771395c782a6def741f
ultron682 <52131708+ultron682@users.noreply.github.com>
2024-08-13 22:50:21 +0200

    Microsoft.AspNetCore.Authentication.JwtBearer
    System.IdentityModel.Tokens.Jwt

--- CodeShareBackend/Program.cs
+++ CodeShareBackend/Program.cs
@@ -37,6 +37,26 @@ builder.Services.AddIdentity<User, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
+// Configure JWT authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
 builder.Services.Configure<IdentityOptions>(options =>
 {
     // Password settings.
