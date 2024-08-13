fbfa3ef02c82fa389fc793001722eaaa44077927
ultron682 <52131708+ultron682@users.noreply.github.com>
2024-08-13 22:47:28 +0200

    JwtTokenGenerator class

---
+++ CodeShareBackend/Helpers/JwtTokenGenerator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace CodeShareBackend.Helpers
+{
+    public static class JwtTokenGenerator
+    {
+        public static string GenerateToken(string email, SymmetricSecurityKey key, string issuer, string audience)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
